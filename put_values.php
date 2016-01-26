806b07703247ce20cfd21334ff4b62362fbb5d5c
root <root@opcserver1>
2016-01-25 22:27:55 -0600

    Added a quick numeric check for free form entry on the website. Should help with command injection

--- put_values.php
+++ put_values.php
@@ -4,6 +4,9 @@ $var = $_GET['var'];
 $value = $_GET['value'];
 
 
+if(!is_numeric($value))
+	$value = 0;
+
 exec("python py-bin/opc_put_values.py ".$var." ".$value." 2>&1", $output, $return_var);
 
 ?>
