1363d420ba910aa78bf35cb9dc732e455b401b79
trohrt <trohrt@quarkfoundation.org>
2016-02-12 18:21:09 -0600

    Update a whole bunch of stuff, just more general cleanup

---
+++ py-bin/config.def.py
@@ -0,0 +1,18 @@
+
+tags=['relay1_breaker',
+	'relay1_flow',
+	'relay1_load',
+	'relay2_breaker',
+	'relay2_flow',
+	'relay2_load',
+	'gen1_breaker',
+	'gen1_generation',
+	'gen2_breaker',
+	'gen2_generation']
+paths = {
+	"substation": "http://0.0.0.0:8000",
+	"relay1": "http://0.0.0.0:8000",
+	"relay2": "http://0.0.0.0:8000",
+	"gen1": "http://0.0.0.0:8000",
+	"gen2": "http://0.0.0.0:8000",
+}
